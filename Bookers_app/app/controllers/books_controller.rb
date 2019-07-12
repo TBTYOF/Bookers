222fd8c6ec9367a1a3bfbe5bb3765f7b582d8180
TBATYOF <toribatyof@gmail.com>
2019-07-12 10:49:42 +0900

    [Add]インデックス機能完成

--- Bookers_app/app/controllers/books_controller.rb
+++ Bookers_app/app/controllers/books_controller.rb
@@ -4,11 +4,24 @@ class BooksController < ApplicationController
 
   def index
   	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book), notice: 'Book was successfully created.'
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
